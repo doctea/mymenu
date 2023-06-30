7463cd6537ac81799356154ea1ed360d564e3c68
doctea <doctea@gmail.com>
2023-06-30 22:26:43 +0100

    compile without screen enabled

--- src/tft.cpp
+++ src/tft.cpp
@@ -1,14 +1,21 @@
-#include "menu.h"
+#ifdef ENABLE_SCREEN
+    #include "menu.h"
+#endif
+#include <Arduino.h>
 
 void tft_print (const char *text) {
     Serial.print(text);
-    Serial_flush();
-    if (menu!=nullptr && menu->tft!=nullptr)
-        menu->tft->print(text);
-    Debug_printf("just did tft print '%s'\n", text); 
-    Serial_flush();
+    //Serial_flush();
+    #ifdef ENABLE_SCREEN
+        if (menu!=nullptr && menu->tft!=nullptr)
+            menu->tft->print(text);
+    #endif
+    //Debug_printf("just did tft print '%s'\n", text); 
+    //Serial_flush();
 }
 void tft_clear() {
-    if (menu!=nullptr && menu->tft!=nullptr)
-        menu->tft->clear();
+    #ifdef ENABLE_SCREEN
+        if (menu!=nullptr && menu->tft!=nullptr)
+            menu->tft->clear();
+    #endif
 }
