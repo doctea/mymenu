9e1154bbf38a2b316f204f8101a8004a65c7aaee
doctea <doctea@gmail.com>
2024-09-21 01:08:38 +0100

    CustomQuickPagesMenuItem to allow for eg Behaviours quickjump

--- include/menuitems_quickpage.h
+++ include/menuitems_quickpage.h
@@ -61,3 +61,72 @@ class QuickPagesMenuItem : public MenuItem {
         }
 
 };
+
+
+class CustomQuickPagesMenuItem : virtual public QuickPagesMenuItem {
+    public:
+        LinkedList<page_t*> *pages = new LinkedList<page_t*>();
+
+        CustomQuickPagesMenuItem(const char *label) : QuickPagesMenuItem(label) {}
+
+        int selected_value_index = -1;
+        //int start_at = 0;
+
+        virtual void add_page(page_t *p) {
+            for (int i =0 ; i < pages->size() ; i++) {
+                if (pages->get(i)==p)   // already exists 
+                    return;
+            }
+            pages->add(p);
+        }
+
+        virtual int display(Coord pos, bool selected, bool opened) override {
+            pos.y = header(label, pos, selected, opened);
+            tft->setTextSize(2);
+
+            uint8_t size = pages->size();
+
+            if (selected_value_index<0 && size>0) {
+                selected_value_index = 0;
+            }
+            for (int i = 0 ; i < size ; i++) {
+                int o = (i + selected_value_index) % size;
+                page_t *page = pages->get(o);
+                if (page==nullptr) 
+                    continue;
+
+                colours(opened && o==selected_value_index, page->colour);
+                tft->println(page->title);
+                colours(C_WHITE);
+
+                if (tft->getCursorY()>=tft->height())
+                    break;
+            }
+            return pos.y;
+        }
+
+        virtual bool knob_left() override {
+            selected_value_index--;
+            if (selected_value_index < 0)
+                selected_value_index = pages->size()-1;
+            return true;
+        }
+
+        virtual bool knob_right() override {
+            selected_value_index++;
+            if (selected_value_index >= (int)pages->size())
+                selected_value_index = 0;
+            return true;
+        }
+
+        virtual bool button_select() override {
+            if (selected_value_index<0)
+                return false;
+            if(Serial && this->debug) Serial.printf("button_select for value %i (%i, %s)\n", selected_value_index, pages->get(selected_value_index), menu->get_quick_page(selected_value_index)->title);
+            // todo: why isn't this properly opening the page and selecting first item?!
+            menu->open_page(menu->get_page_index_for_name(pages->get(selected_value_index)->title));
+            //page_t * selected_page = menu->get_selected_page();
+            return false;
+        }
+
+};
